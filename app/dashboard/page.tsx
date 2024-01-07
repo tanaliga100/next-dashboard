581668a5b47d445bdd3eff6e991a6fb169410dc7
tanaliga100 <jordantanaliga@gmail.com>
2024-01-07 14:23:04 +0800

    chapter-7

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,40 @@
-export default function Page() {
-  return <h1> Dashboard Page</h1>;
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import { Card } from '../ui/dashboard/cards';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfCustomers,
+    numberOfInvoices,
+  } = await fetchCardData();
+
+  // const totalPaidInvoices = 130;
+  // const totalPendingInvoices = 150;
+  // const numberOfInvoices = 200;
+  // const numberOfCustomers = 300;
+
+  return (
+    <main>
+      <h1> Dashboard Page</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
